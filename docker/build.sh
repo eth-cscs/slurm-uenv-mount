597ab537d3f73a0051267624ea4189c11269fff7
Simon Pintarelli <simon.pintarelli@cscs.ch>
2022-10-31 11:41:55 +0100

    update build script

--- docker/build.sh
+++ docker/build.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/bash
 
+TAG=centos-slurm-uenv-mount
+
 SOURCE_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
 (
     cd ../ || exit 1
-    BUILDKIT_PROGRESS=plain docker build . -f "${SOURCE_DIR}/Dockerfile" --progress=plain -t centos-slurm-uenv-mount
+    BUILDKIT_PROGRESS=plain docker build . -f "${SOURCE_DIR}/Dockerfile" --progress=plain -t $TAG
 )
+
+docker tag $TAG simonpzh/$TAG
+
+docker push simonpzh/$TAG
