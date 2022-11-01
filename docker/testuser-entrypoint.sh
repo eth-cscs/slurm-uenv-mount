b80d29afa04f9f0c6e815784c5e53d784f62a2d4
Simon Pintarelli <simon.pintarelli@cscs.ch>
2022-11-01 16:05:54 +0100

    add a stub sqfs file for testuser

--- docker/testuser-entrypoint.sh
+++ docker/testuser-entrypoint.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+set -x
+
 . /usr/lib64/mpi/gcc/mpich/bin/mpivars.sh
+
+2>&1 > /dev/null su testuser -c sh <<\EOF
+dd=$(mktemp -d)
+
+(
+    umask 022
+    cd "$dd" || exit
+    mkdir test
+    echo "This is file A" >> test/fileA.txt
+    mkdir -p test/testdir
+)
+
+if command -v tree
+then
+    tree "$dd"
+fi
+
+mksquashfs  "$dd"  /home/testuser/fs.sqfs -noappend && rm -r "$dd"
+EOF
 
 exec /entrypoint.sh su testuser -s /bin/bash -l
