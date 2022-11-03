9dc38cbf49daf522b62bf2e2aca72b789c32429d
Simon Pintarelli <simon.pintarelli@cscs.ch>
2022-11-03 16:27:06 +0100

    merge entrypoint and dockerfile

--- docker/testuser-entrypoint.sh
+++ docker/testuser-entrypoint.sh
@@ -23,4 +23,9 @@ fi
 mksquashfs  "$dd"  /home/testuser/fs.sqfs -noappend && rm -r "$dd"
 EOF
 
+echo "To test plugin try:"
+echo
+echo "srun -t 10  --uenv-mount-file=fs.sqfs ls"
+echo
+
 exec /entrypoint.sh su testuser -s /bin/bash -l
