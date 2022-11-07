93a57e5345c1d453cb03b23c4d76269c7b4b1b01
Jonathan Coles <jonathan@jpcoles.com>
2022-11-08 00:54:08 +0100

    Update testuser-entrypoint.sh

--- ci/testuser-entrypoint.sh
+++ ci/testuser-entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+set -x
+
 # Switch to the user "testuser" to run the rest of the script
 if [ "$UID" = "0" ]; then
     #cat /var/log/slurm/slurmctld.log
     #cat /var/log/slurm/slurmd.nd00001.log
     #cat /var/log/slurm/slurmd.nd00002.log
     #cat /var/log/slurm/slurmd.nd00003.log
-    su --whitelist-environment=SLURM_ROOT -s /bin/bash - testuser -c "$0 $@"
+    su --whitelist-environment=SLURM_ROOT -s /bin/bash - testuser -- $0 "$@"
     exit
 fi
 
