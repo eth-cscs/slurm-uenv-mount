6bfccc0fd7d23a10164ab8132f0e670bb0b06a37
Jonathan Coles <jonathan.coles@cscs.ch>
2022-11-07 17:04:31 +0100

    o Change the default Docker files to what was previously marked with 'jpc' o Prepare the ci directory for running github tests.

--- docker/testuser-entrypoint-simonpzh.sh
+++ docker/testuser-entrypoint-simonpzh.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 . /usr/lib64/mpi/gcc/mpich/bin/mpivars.sh
 
 2>&1 > /dev/null su testuser -c sh <<\EOF
@@ -21,12 +23,4 @@ fi
 mksquashfs  "$dd"  /home/testuser/fs.sqfs -noappend && rm -r "$dd"
 EOF
 
-
-. /entrypoint.sh 
-
-echo "To test plugin try:"
-echo
-echo "srun -t 10  --uenv-mount-file=0.1.105.tar.gz ls"
-echo
-
-su testuser -s /bin/bash -l
+exec /entrypoint.sh su testuser -s /bin/bash -l
