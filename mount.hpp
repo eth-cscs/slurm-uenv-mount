89357eb01da6223675cd32be486c4ff62665608d
Simon Pintarelli <simon.pintarelli@cscs.ch>
2022-12-13 23:33:04 +0100

    change env vars SLURM_UENV_MOUNT_FILE|POINT -> UENV_MOUNT_FILE|POINT

--- mount.hpp
+++ mount.hpp
@@ -2,8 +2,8 @@ extern "C" {
 #include <slurm/spank.h>
 }
 
-#define ENV_MOUNT_FILE "SLURM_UENV_MOUNT_FILE"
-#define ENV_MOUNT_POINT "SLURM_UENV_MOUNT_POINT"
+#define ENV_MOUNT_FILE "UENV_MOUNT_FILE"
+#define ENV_MOUNT_POINT "UENV_MOUNT_POINT"
 // #define ENV_MOUNT_SKIP_PROLOGUE "SLURM_UENV_MOUNT_PROLOGUE"
 
 namespace impl {
