5162fd1fff95ea6b14128f4b9ce38931d0ef5b05
Simon Pintarelli <simon.pintarelli@cscs.ch>
2022-12-17 10:57:05 +0100

    add tests

---
+++ ci/tests/squashfs-run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+# pretend to run inside squasfsh-run ... bash
+export UENV_MOUNT_FILE=/home/testuser/fs.sqfs
+export UENV_MOUNT_POINT=/tmp
+
+srun stat /tmp/test
+
+# use --uenv-mount-file to ovveride UENV_MOUNT_FILE
+# expectation: squahfs is mounted in `/user-environment/` (default location)
+srun --uenv-mount-file=/home/testuser/fs2.sqfs stat /user-environment/test/fileB.txt
