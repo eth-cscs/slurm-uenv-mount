a1daa9de47727acc6d588d5a28ac5b9c13a78a69
Simon Pintarelli <simon.pintarelli@cscs.ch>
2022-12-24 01:13:09 +0100

    docker-env: specify home directory

--- docker/run.sh
+++ docker/run.sh
@@ -3,5 +3,5 @@
 
 SOURCE_DIR=$(realpath $(dirname "$(realpath "${BASH_SOURCE[0]}")")/../)
 
-docker run --name slurm-uenv-mount --volume="${SOURCE_DIR}":/slurm-uenv-mount:ro \
-       --rm --privileged -it slurm-uenv-mount bash
+docker run --name slurm-uenv-mount -w /home/testuser --volume="${SOURCE_DIR}":/slurm-uenv-mount:ro \
+       --rm --privileged -it slurm-uenv-mount
