560a603f7388d0fca3594d78a2e6a2afa7c3889c
Simon Pintarelli <simon.pintarelli@cscs.ch>
2023-01-07 10:15:28 +0100

    invoke new tests in docker/run-tests.sh

--- docker/run-tests.sh
+++ docker/run-tests.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-echo "--------------------------------"
-echo "Test --uenv-mount-file in sbatch"
-echo "--------------------------------"
-sbatch --wait /slurm-uenv-mount/ci/tests/test.sbatch
+set -xe
+
+echo "for loop"
+echo "$(find /slurm-uenv-mount/ci/tests -iname '*.sbatch')"
+for sbatch_input in $(find /slurm-uenv-mount/ci/tests -iname '*.sbatch');
+do
+    echo "sbatch --wait ${sbatch_input}"
+    sbatch --wait "${sbatch_input}"
+done
+
+while IFS= read -r -d '' file
+do
+    sbatch --wait "${file}"
+
+done <   <(find /slurm-uenv-mount/ci/tests -iname '*.sbatch' -print0)
+
 
 echo "-----------------------------------------------------------------------"
 echo "test inheritance of UENV_MOUNT_FILE/POINT in dummy squashfs-run session"
