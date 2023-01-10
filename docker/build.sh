9872c39a77f0929bd2ac4e0af1a68d9e1ea9b8c7
Jonathan Coles <jonathan.coles@cscs.ch>
2023-01-10 08:47:41 +0100

    Remove meson (and python) dependency.

--- docker/build.sh
+++ docker/build.sh
@@ -3,10 +3,17 @@
 set -x
 
 TAG=slurm-uenv-mount
+CONTAINER=$1
 
-SOURCE_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+if [ ! -z "$CONTAINER" ]; then
+    BUILD_ARGS+="--build-arg DOCKER_CONTAINER=${CONTAINER}"
+fi
+
+#SOURCE_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)"
+
 
 (
     cd ../ || exit 1
-    BUILDKIT_PROGRESS=plain docker build . -f "${SOURCE_DIR}/Dockerfile" --progress=plain -t $TAG
+    BUILDKIT_PROGRESS=plain docker build . -f "${SOURCE_DIR}/Dockerfile" ${BUILD_ARGS} --progress=plain -t $TAG
 )
--- docker/run.sh
+++ docker/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-SOURCE_DIR=$(realpath $(dirname "$(realpath "${BASH_SOURCE[0]}")")/../)
+SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)"
 
 docker run --name slurm-uenv-mount -w /home/testuser --volume="${SOURCE_DIR}":/slurm-uenv-mount:ro \
        --rm --privileged -it slurm-uenv-mount
