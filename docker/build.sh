2e5ededa28d2d048afe61b2e3aab014907351372
bcumming <bcumming@cscs.ch>
2023-02-01 09:12:41 +0100

    fix container build script

--- docker/build.sh
+++ docker/build.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/bash
 
-set -x
-
 TAG=slurm-uenv-mount
 CONTAINER=$1
 
@@ -9,9 +7,17 @@ if [ ! -z "$CONTAINER" ]; then
     BUILD_ARGS+="--build-arg DOCKER_CONTAINER=${CONTAINER}"
 fi
 
-SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"/../; pwd)"
+DOCKER_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"/; pwd)"
+SOURCE_DIR=$(realpath "${DOCKER_DIR}/..")
+
+echo
+echo container $CONTAINER
+echo tag $TAG
+echo source $SOURCE_DIR
+echo docker $DOCKER_DIR
+echo
 
 (
-    cd ../ || exit 1
-    BUILDKIT_PROGRESS=plain docker build . -f "${SOURCE_DIR}/Dockerfile" ${BUILD_ARGS} --progress=plain -t $TAG
+    cd ${SOURCE_DIR} || exit 1
+    BUILDKIT_PROGRESS=plain docker build . -f "${DOCKER_DIR}/Dockerfile" ${BUILD_ARGS} --progress=plain -t $TAG
 )
