cde256fc46269df23c8c973d38f08e6f7bd5f6df
Simon Pintarelli <simon.pintarelli@cscs.ch>
2023-02-20 09:23:00 +0100

    make-binary-rpm use same naming scheme as in ci

--- docker/make-binary-rpm.sh
+++ docker/make-binary-rpm.sh
@@ -8,7 +8,14 @@ cd /tmp/build-rpm
 _SLURM_VER=$(srun --version | sed 's/slurm //')
 make -f /slurm-uenv-mount/Makefile RPM_SLURM_VERSION="${_SLURM_VER}" rpm
 rpmbuild --rebuild "$(find rpm/SRPMS -name '*.rpm' -type f -print -quit)" --define "_topdir $(pwd)/rpm"
+
+rm ./*.rpm
 find rpm -name '*.rpm' -type f -exec cp {} . \;
+
+fname=$(ls *x86_64.rpm)
+dstname=$(echo $fname | sed "s/x86_64/x86_64-slurm-${_SLURM_VER}/")
+mv $fname $dstname
+
 
 echo
 echo "RPM built. "
@@ -16,6 +23,6 @@ echo
 
 echo "Copy from container:"
 echo
-printf "\tdocker compose cp slurm:$(realpath *x86_64.rpm) ."
+printf "\tdocker compose cp slurm:$(realpath $dstname) ."
 echo
 echo
